19120db40a108e31ea35f1dd7c7d4c1fd6ae9577
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-04 13:23:03 -0300

    feat: seed data

--- src/main/java/com/github/qualquercoisavinteconto/seeds/DatabaseSeeder.java
+++ src/main/java/com/github/qualquercoisavinteconto/seeds/DatabaseSeeder.java
@@ -3,28 +3,41 @@ package com.github.qualquercoisavinteconto.seeds;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.github.qualquercoisavinteconto.models.Role;
+import com.github.qualquercoisavinteconto.models.User;
 import com.github.qualquercoisavinteconto.repositories.RoleRepository;
+import com.github.qualquercoisavinteconto.repositories.UserRepository;
 
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
 
-    public DatabaseSeeder(RoleRepository roleRepository) {
+    public DatabaseSeeder(RoleRepository roleRepository, UserRepository userRepository) {
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        if (roleRepository.count() == 0) {
-            roleRepository.saveAll(List.of(
+        if (this.roleRepository.count() == 0) {
+            this.roleRepository.saveAll(List.of(
                     new Role("ADMIN"),
                     new Role("CUSTOMER"),
                     new Role("SELLER"),
                     new Role("ADVERTISER")));
+        }
+
+        if (this.userRepository.count() == 0) {
+            String encryptedPassword = new BCryptPasswordEncoder().encode("123456");
+            User user = new User("Admin Tester", "tester@admin.com", encryptedPassword);
+            Role role = roleRepository.getReferenceById(1L);
+            user.setRoles(List.of(role));
+            this.userRepository.save(user);
         }
     }
 
