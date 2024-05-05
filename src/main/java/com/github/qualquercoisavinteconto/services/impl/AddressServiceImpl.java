770222264a20c490bbf11cfbe3a1ac2a855bf0b7
Matheus Queiroz <137003929+matheusqap@users.noreply.github.com>
2024-05-05 15:40:44 -0300

    AddressServiceImpl

---
+++ src/main/java/com/github/qualquercoisavinteconto/services/impl/AddressServiceImpl.java
@@ -0,0 +1,41 @@
+package com.github.qualquercoisavinteconto.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.github.qualquercoisavinteconto.models.User;
+import com.github.qualquercoisavinteconto.models.Address;
+import com.github.qualquercoisavinteconto.repositories.UserRepository;
+import com.github.qualquercoisavinteconto.repositories.AddressRepository;
+import com.github.qualquercoisavinteconto.services.AddressService;
+import com.github.qualquercoisavinteconto.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressRepository addressRepository;
+
+    @Override
+    public Address save(Address address)
+    {
+        return addressRepository.save(address);
+    }
+
+    @Override
+    public Address findById(Long id)
+    {
+        return addressRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Address> finAddressesByUser(User user)
+    {
+        return addressRepository.findByUser(user);
+    }
+    
+}
