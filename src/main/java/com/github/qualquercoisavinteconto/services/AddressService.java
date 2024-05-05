337f672dfb79259d368239120fe7ad77afc14220
Matheus Queiroz <137003929+matheusqap@users.noreply.github.com>
2024-05-05 15:39:44 -0300

    Address Service

---
+++ src/main/java/com/github/qualquercoisavinteconto/services/AddressService.java
@@ -0,0 +1,15 @@
+package com.github.qualquercoisavinteconto.services;
+
+import java.util.List;
+
+import com.github.qualquercoisavinteconto.models.Address;
+import com.github.qualquercoisavinteconto.models.User;
+
+public interface AddressService {
+
+    Address save(Address address);
+    Address findById(Long id);
+    List<Address> finAddressesByUser(User user);
+
+} 
+
