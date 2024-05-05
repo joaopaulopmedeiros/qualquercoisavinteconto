8d4bbb7169f9bb8808c1a80e0a83da16c090f983
Matheus Queiroz <137003929+matheusqap@users.noreply.github.com>
2024-05-05 15:33:15 -0300

    Update AddressRepository.java

--- src/main/java/com/github/qualquercoisavinteconto/repositories/AddressRepository.java
+++ src/main/java/com/github/qualquercoisavinteconto/repositories/AddressRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.qualquercoisavinteconto.models.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long>{
+
+  List<Address> findByUser(User user);
   
 }
