e854331e8cac86b7f93f94375f5e65e09dfe3325
andrefernandeslp1 <andre.mdia@gmail.com>
2024-05-10 16:08:02 -0300

    feat: AddressDTO, AddressController

--- src/main/java/com/github/qualquercoisavinteconto/controllers/AddressController.java
+++ src/main/java/com/github/qualquercoisavinteconto/controllers/AddressController.java
@@ -2,17 +2,52 @@ package com.github.qualquercoisavinteconto.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import com.github.qualquercoisavinteconto.services.ProductService;
+import com.github.qualquercoisavinteconto.services.AddressService;
+import com.github.qualquercoisavinteconto.services.UserService;
+import com.github.qualquercoisavinteconto.models.Address;
+import com.github.qualquercoisavinteconto.dto.AddressDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 
 @RestController
 @Tag(name = "Address")
-@RequestMapping("address")
+@RequestMapping("/address")
 public class AddressController {
-    private final ProductService service;
+    // private final ProductService service;
 
-    public AddressController(ProductService service) {
-        this.service = service;
+    // public AddressController(ProductService service) {
+    //     this.service = service;
+    // }
+
+    @Autowired
+    AddressService addressService;
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Address save(@RequestBody AddressDTO addressDTO) {
+        Address address = new Address();
+        address.setCity(addressDTO.getCity());
+        address.setNumber(addressDTO.getNumber());
+        address.setState(addressDTO.getState());
+        address.setStreet(addressDTO.getStreet());
+        address.setUser(userService.findById(addressDTO.getUser_id()));
+        return addressService.save(address);
     }
+
+
+
+
+
 }
---
+++ src/main/java/com/github/qualquercoisavinteconto/dto/AddressDTO.java
@@ -0,0 +1,28 @@
+package com.github.qualquercoisavinteconto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/* Body da requisição:
+{
+  "user_id": 1,
+  "street": "Rua de teste",
+  "number": "123",
+  "state": "SP",
+  "city": "São Paulo",
+}
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
+
+    private Long user_id;
+    private String street;
+    private String number;
+    private String state;
+    private String city;
+  
+}
