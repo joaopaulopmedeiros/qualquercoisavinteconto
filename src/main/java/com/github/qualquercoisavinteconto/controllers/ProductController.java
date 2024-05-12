9c0de20c8820490b7f8897a8cec3cf2341520e14
Marcio Victor <marcio9769@gmail.com>
2024-05-12 14:57:35 -0300

    feat: implementado endpoints de product(com bug)

--- src/main/java/com/github/qualquercoisavinteconto/controllers/ProductController.java
+++ src/main/java/com/github/qualquercoisavinteconto/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.github.qualquercoisavinteconto.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.qualquercoisavinteconto.dto.ProductDTO;
+import com.github.qualquercoisavinteconto.models.Product;
 import com.github.qualquercoisavinteconto.requests.ProductSearchRequest;
 import com.github.qualquercoisavinteconto.responses.ProductSearchResponse;
 import com.github.qualquercoisavinteconto.services.ProductService;
@@ -9,7 +11,12 @@ import com.github.qualquercoisavinteconto.services.ProductService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -25,5 +32,26 @@ public class ProductController {
     @GetMapping
     public List<ProductSearchResponse> search(ProductSearchRequest request) {
         return this.service.search(request);
+    }
+
+    @GetMapping("/id")
+    public Product findById(Long id) {
+        return this.service.findById(id);
+    }
+
+    @PostMapping
+    public Product save(ProductDTO product) {
+        System.out.println(product.getName() + product.getPrice());
+        return this.service.save(product);
+    }
+
+    @PutMapping("/id")
+    public void update(@PathVariable Long id, ProductDTO productDTO) {
+        this.service.update(id, productDTO);
+    }
+
+    @DeleteMapping("/id")
+    public void delete(@PathVariable Long id) {
+        this.service.delete(id);
     }
 }
--- src/main/java/com/github/qualquercoisavinteconto/services/ProductService.java
+++ src/main/java/com/github/qualquercoisavinteconto/services/ProductService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.github.qualquercoisavinteconto.dto.ProductDTO;
 import com.github.qualquercoisavinteconto.mappers.ProductMapper;
 import com.github.qualquercoisavinteconto.models.Product;
 import com.github.qualquercoisavinteconto.repositories.ProductRepository;
@@ -31,5 +32,22 @@ public class ProductService {
         return products.stream()
             .map(ProductMapper::mapToSearchResponse)
             .collect(Collectors.toList());
+    }
+
+    public Product save(ProductDTO product) {
+        Product newProduct = new Product();
+        newProduct.setName(product.getName());
+        newProduct.setPrice(product.getPrice());
+        return repository.save(newProduct);
+    }
+
+    public void update(Long id, ProductDTO productDTO) {
+        Product product = repository.findById(id).orElseThrow();
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
