4874ce5d277d1caa948d64408839f11ccf18ae07
Matheus Queiroz <matheusqap@gmail.com>
2024-05-17 00:44:58 -0300

    edit> user controller

--- src/main/java/com/github/qualquercoisavinteconto/controllers/UserController.java
+++ src/main/java/com/github/qualquercoisavinteconto/controllers/UserController.java
@@ -3,6 +3,15 @@ package com.github.qualquercoisavinteconto.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.qualquercoisavinteconto.dto.AddressDTO;
+import com.github.qualquercoisavinteconto.dto.UserDTO;
+import com.github.qualquercoisavinteconto.exceptions.ResourceNotFoundException;
+import com.github.qualquercoisavinteconto.models.Address;
+import com.github.qualquercoisavinteconto.models.Product;
+import com.github.qualquercoisavinteconto.models.User;
+import com.github.qualquercoisavinteconto.requests.ProductRequest;
+import com.github.qualquercoisavinteconto.requests.ProductSearchRequest;
+import com.github.qualquercoisavinteconto.responses.ProductSearchResponse;
 import com.github.qualquercoisavinteconto.responses.UserSearchResponse;
 import com.github.qualquercoisavinteconto.services.UserService;
 
@@ -10,8 +19,16 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController
@@ -31,5 +48,38 @@ public class UserController {
         var result = this.service.findAll();
         if (result == null || result.isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var result = service.findById(id);
+        if (result == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(result);  
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User save(@RequestBody UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setRoles(userDTO.getRoles());
+        return service.save(user);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody UserDTO userDTO) throws ResourceNotFoundException {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setRoles(userDTO.getRoles());
+        service.save(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) throws ResourceNotFoundException {
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
