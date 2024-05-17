086223f63f75bd24b096d1fc25f8cdb83f4914d2
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-17 00:49:17 -0300

    refactor: add Repository decorator

--- src/main/java/com/github/qualquercoisavinteconto/repositories/UserRepository.java
+++ src/main/java/com/github/qualquercoisavinteconto/repositories/UserRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import com.github.qualquercoisavinteconto.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     UserDetails findUserDetailsByEmail(String email);
