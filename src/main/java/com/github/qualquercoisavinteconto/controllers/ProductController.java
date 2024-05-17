f792ff431a57ccf864e6d907d7c28b4849e04c99
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-16 22:47:10 -0300

    refactor: code clean up

--- src/main/java/com/github/qualquercoisavinteconto/controllers/ProductController.java
+++ src/main/java/com/github/qualquercoisavinteconto/controllers/ProductController.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
