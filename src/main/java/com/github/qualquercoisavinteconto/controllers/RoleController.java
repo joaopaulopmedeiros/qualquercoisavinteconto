ec6f8d638cf1203a63de65abae956e9e90725a7d
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-17 00:36:33 -0300

    feat: role search

--- src/main/java/com/github/qualquercoisavinteconto/controllers/RoleController.java
+++ src/main/java/com/github/qualquercoisavinteconto/controllers/RoleController.java
@@ -1,8 +1,13 @@
 package com.github.qualquercoisavinteconto.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.qualquercoisavinteconto.models.Role;
 import com.github.qualquercoisavinteconto.services.RoleService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -14,5 +19,12 @@ public class RoleController {
 
     public RoleController(RoleService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Role>> search() {
+        var result = this.service.findAll();
+        if (result == null || result.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(result);
     }
 }
