e438bd5b2d2aa049d70f5947677a465943c07ad0
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-16 23:21:54 -0300

    refactor: code clean up

--- src/main/java/com/github/qualquercoisavinteconto/services/ProductService.java
+++ src/main/java/com/github/qualquercoisavinteconto/services/ProductService.java
@@ -1,11 +1,8 @@
 package com.github.qualquercoisavinteconto.services;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.github.qualquercoisavinteconto.exceptions.ResourceNotFoundException;
