fa402a95cc2dda67d51821ba23a371da1fac9777
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-19 22:09:29 -0300

    refactor: remove unused imports

--- src/main/java/com/github/qualquercoisavinteconto/controllers/ReviewController.java
+++ src/main/java/com/github/qualquercoisavinteconto/controllers/ReviewController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.qualquercoisavinteconto.services.ProductService;
-import com.github.qualquercoisavinteconto.services.ReviewService;
-import com.github.qualquercoisavinteconto.services.UserService;
 import com.github.qualquercoisavinteconto.services.impl.ReviewServiceImpl;
 import com.github.qualquercoisavinteconto.models.Review;
 import com.github.qualquercoisavinteconto.dto.ReviewDTO;
